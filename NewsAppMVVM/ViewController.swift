ff01e979a90549d38a19b11e225d36a3a6dc4510
John Varghese 2 <john_varghese2@intuit.com>
2020-03-02 09:53:03 -0800

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Varghese, John on 3/2/20.
+//  Copyright © 2020 Intuit, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
