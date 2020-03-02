602397877be415dbcf5e3b453cad65543a4eed1a
John Varghese 2 <john_varghese2@intuit.com>
2020-03-02 11:27:36 -0800

    built a view models to represent an article and a list of articles

---
+++ NewsAppMVVM/View Models/ArticleViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ArticleViewModel.swift
+//  NewsAppMVVM
+//
+//  Created by Varghese, John on 3/2/20.
+//  Copyright © 2020 Intuit, Inc. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+struct ArticleListViewModel {
+    let articlesVM: [ArticleViewModel]
+}
+
+extension ArticleListViewModel {
+    init(_ articles: [Article]) {
+        self.articlesVM = articles.compactMap(ArticleViewModel.init)
+    }
+}
+
+extension ArticleListViewModel {
+    func articleAt(_ index: Int) -> ArticleViewModel {
+        return self.articlesVM[index]
+    }
+}
+
+struct ArticleViewModel {
+    let article: Article
+    
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+extension ArticleViewModel {
+    var title: Observable<String> {
+        return Observable<String>.just(article.title)
+    }
+    
+    var description: Observable<String> {
+        return Observable<String>.just(article.description ?? "")
+    }
+}
